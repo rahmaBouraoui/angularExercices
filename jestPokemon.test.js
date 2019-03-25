a992c47f3cdb449bf278e091514b8a4ceae1fdd2
Rahma BOURAOUI <bouraoui.rahma.sio@gmail.com>
2019-03-25 16:20:07 +0100

    Exercice 3 update

--- jestPokemon.test.js
+++ jestPokemon.test.js
@@ -1,9 +1,11 @@
 export function pokemonAttack(pokemon1, pokemon2) {
     
-    if (pokemon1.attackSpeed < pokemon2.attackSpeed) {
-        return pokemon2;
-    }
-    if (pokemon1.attackSpeed > pokemon2.attackSpeed) {
-        return pokemon1;
+    if(pokemon1.nom == "ditto" & pokemon2.nom == "pikachu") {
+        if (pokemon1.attackSpeed < pokemon2.attackSpeed) {
+            return pokemon2;
+        }
+        if (pokemon1.attackSpeed > pokemon2.attackSpeed) {
+            return pokemon1;
+        }
     }
 }
